3cc6f1be3141be1d6cf31a9326aedb810776186a
Xianzheng Zhou <xianzheng.zhou@outlook.com>
2019-03-27 14:17:49 +0800

    fix perl scripts

--- config.pl
+++ config.pl
@@ -2,10 +2,10 @@ use strict;
 use Config::Properties;
 
 # reading...
-my $config_file = 'config/server.properties'
+my $config_file = 'config/server.properties';
 
 if ($ENV{'CONFIG_FILE'}) {
-  $config_file = $ENV{'CONFIG_FILE'}
+  $config_file = $ENV{'CONFIG_FILE'};
 }
 open my $fh, '<', $config_file
   or die "unable to default open configuration file";
@@ -13,18 +13,16 @@ open my $fh, '<', $config_file
 my $properties = Config::Properties->new();
 $properties->load($fh);
 
-$value = $properties->getProperty($key);
-
 if ($ENV{'BROKER_ID'}) {
-  $properties->setProperty('broker.id', $ENV{'BROKER_ID'})
+  $properties->setProperty('broker.id', $ENV{'BROKER_ID'});
 }
 
 if ($ENV{'LOG_DIRS'}) {
-  $properties->setProperty('log.dirs', $ENV{'LOG_DIRS'})
+  $properties->setProperty('log.dirs', $ENV{'LOG_DIRS'});
 }
 
 if ($ENV{'ZK_HOSTS'}) {
-  $properties->setProperty('zookeeper.connect', $ENV{'ZK_HOSTS'})
+  $properties->setProperty('zookeeper.connect', $ENV{'ZK_HOSTS'});
 }
 
 # saving...
