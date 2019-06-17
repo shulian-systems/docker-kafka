5c21e462c2351ef09f677e1d7342e892df8c8a67
Xianzheng Zhou <xianzheng.zhou@outlook.com>
2019-06-17 14:58:45 +0800

    missing ;

--- config.pl
+++ config.pl
@@ -22,7 +22,7 @@ if ($ENV{'ZK_HOSTS'}) {
 }
 
 if ($ENV{'EXTERNAL_PORT_BASE'} && $ENV{'EXTERNAL_HOST'}) {
-  my $external_port = int($ENV{'EXTERNAL_PORT_BASE'}) + int($ENV{'BROKER_ID'})
+  my $external_port = int($ENV{'EXTERNAL_PORT_BASE'}) + int($ENV{'BROKER_ID'});
   $properties->setProperty('advertised.listeners', $ENV{'PLAINTEXT://'.$ENV{'EXTERNAL_HOST'}.':'.$external_port});
 }
 $properties->setProperty('log.dirs', '/data');
