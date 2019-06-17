3191cead61859ad0588a98565c2208b47bf8bcad
Xianzheng Zhou <xianzheng.zhou@outlook.com>
2019-06-17 14:30:36 +0800

    external advertised url

--- config.pl
+++ config.pl
@@ -20,6 +20,11 @@ if ($ENV{'BROKER_ID'}) {
 if ($ENV{'ZK_HOSTS'}) {
   $properties->setProperty('zookeeper.connect', $ENV{'ZK_HOSTS'});
 }
+
+if ($ENV{'EXTERNAL_PORT_BASE'} && $ENV{'EXTERNAL_HOST'}) {
+  my $external_port = int($ENV{'EXTERNAL_PORT_BASE'}) + int($ENV{'BROKER_ID'})
+  $properties->setProperty('advertised.listeners', $ENV{'PLAINTEXT://'.$ENV{'EXTERNAL_HOST'}.':'.$external_port});
+}
 $properties->setProperty('log.dirs', '/data');
 
 # saving...
