ed9ee4c8a71790bd3dc4e2ea1e35ccf8059bacca
Xianzheng Zhou <xianzheng.zhou@outlook.com>
2019-06-18 10:24:53 +0800

    internal hostname calculation

--- config.pl
+++ config.pl
@@ -24,7 +24,8 @@ if ($ENV{'ZK_HOSTS'}) {
 
 if ($ENV{'EXTERNAL_PORT_BASE'} && $ENV{'EXTERNAL_HOST'} && $ENV{'INTERNAL_DOMAIN'}) {
   my $external_port = int($ENV{'EXTERNAL_PORT_BASE'}) + int($ENV{'BROKER_ID'});
-  $properties->setProperty('advertised.listeners', 'INTERNAL://'.hostname.'.'.$ENV{'INTERNAL_DOMAIN'}.':9092,EXTERNAL://'.$ENV{'EXTERNAL_HOST'}.':'.$external_port);
+  my $host = hostname;
+  $properties->setProperty('advertised.listeners', 'INTERNAL://'.$host.'.'.$ENV{'INTERNAL_DOMAIN'}.':9092,EXTERNAL://'.$ENV{'EXTERNAL_HOST'}.':'.$external_port);
   $properties->setProperty('listener.security.protocol.map', 'INTERNAL:PLAINTEXT,EXTERNAL:PLAINTEXT');
   $properties->setProperty('inter.broker.listener.name', 'INTERNAL');
 }
