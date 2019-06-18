3b14545281edc5417820af7b3a7a7314f397d7c2
Xianzheng Zhou <xianzheng.zhou@outlook.com>
2019-06-18 10:13:38 +0800

    internal hostname calculation

--- config.pl
+++ config.pl
@@ -1,5 +1,6 @@
 use strict;
 use Config::Properties;
+use Sys:Hostname;
 
 # reading...
 my $config_file = 'config/server.properties';
@@ -21,9 +22,9 @@ if ($ENV{'ZK_HOSTS'}) {
   $properties->setProperty('zookeeper.connect', $ENV{'ZK_HOSTS'});
 }
 
-if ($ENV{'EXTERNAL_PORT_BASE'} && $ENV{'EXTERNAL_HOST'} && $ENV{'INTERNAL_HOST'}) {
+if ($ENV{'EXTERNAL_PORT_BASE'} && $ENV{'EXTERNAL_HOST'} && $ENV{'INTERNAL_DOMAIN'}) {
   my $external_port = int($ENV{'EXTERNAL_PORT_BASE'}) + int($ENV{'BROKER_ID'});
-  $properties->setProperty('advertised.listeners', 'INTERNAL://'.$ENV{'INTERNAL_HOST'}.':9092,EXTERNAL://'.$ENV{'EXTERNAL_HOST'}.':'.$external_port);
+  $properties->setProperty('advertised.listeners', 'INTERNAL://'.hostname.'.'.$ENV{'INTERNAL_DOMAIN'}.':9092,EXTERNAL://'.$ENV{'EXTERNAL_HOST'}.':'.$external_port);
   $properties->setProperty('listener.security.protocol.map', 'INTERNAL:PLAINTEXT,EXTERNAL:PLAINTEXT');
   $properties->setProperty('inter.broker.listener.name', 'INTERNAL');
 }
