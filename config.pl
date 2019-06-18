28e266f95f76bffc7005832fb37fc4bd559cd2fb
Xianzheng Zhou <xianzheng.zhou@outlook.com>
2019-06-18 14:16:06 +0800

    external port fixed listening on base port number

--- config.pl
+++ config.pl
@@ -26,7 +26,7 @@ if ($ENV{'EXTERNAL_PORT_BASE'} && $ENV{'EXTERNAL_HOST'} && $ENV{'INTERNAL_DOMAIN
   my $external_port = int($ENV{'EXTERNAL_PORT_BASE'}) + int($ENV{'BROKER_ID'});
   $properties->setProperty('advertised.listeners', 'INTERNAL://'.hostname.'.'.$ENV{'INTERNAL_DOMAIN'}.':9092,EXTERNAL://'.$ENV{'EXTERNAL_HOST'}.':'.$external_port);
   $properties->setProperty('listener.security.protocol.map', 'INTERNAL:PLAINTEXT,EXTERNAL:PLAINTEXT');
-  $properties->setProperty('listeners', 'INTERNAL://:9092,EXTERNAL://:'.$external_port);
+  $properties->setProperty('listeners', 'INTERNAL://:9092,EXTERNAL://:'.$ENV{'EXTERNAL_PORT_BASE'});
   $properties->setProperty('inter.broker.listener.name', 'INTERNAL');
 }
 $properties->setProperty('log.dirs', '/data');
